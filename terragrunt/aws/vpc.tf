41b5c771943da43f725764121cf36c159a433f72
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2022-02-11 16:03:55 -0800

    Moving files to the api directory

--- terragrunt/aws/vpc.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.46//vpc"
-  name              = var.product_name
-  billing_tag_value = var.billing_code
-  high_availability = true
-  enable_flow_log   = true
-}
