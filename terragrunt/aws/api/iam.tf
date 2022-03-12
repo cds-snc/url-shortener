3e3b68f7799a8d83331834574dce70f2a57340c8
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2022-03-11 16:38:13 -0800

    Adding waf rules

--- terragrunt/aws/api/iam.tf
+++ terragrunt/aws/api/iam.tf
@@ -25,3 +25,68 @@ resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
+resource "aws_iam_role" "waf_log_role" {
+  name               = "${var.product_name}-logs"
+  assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+resource "aws_iam_policy" "write_waf_logs" {
+  name        = "${var.product_name}_WriteLogs"
+  description = "Allow writing WAF logs to S3 + CloudWatch"
+  policy      = data.aws_iam_policy_document.write_waf_logs.json
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "write_waf_logs" {
+  role       = aws_iam_role.waf_log_role.name
+  policy_arn = aws_iam_policy.write_waf_logs.arn
+}
+
+data "aws_iam_policy_document" "firehose_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "write_waf_logs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      module.log_bucket.s3_bucket_arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject*",
+      "s3:PutObject*",
+    ]
+
+    resources = [
+      "${module.log_bucket.s3_bucket_arn}/waf/*"
+    ]
+  }
+}
