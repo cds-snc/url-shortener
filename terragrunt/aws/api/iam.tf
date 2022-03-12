38cb7580aa715319a6c010179400d142a5ed5c49
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2022-03-11 16:06:37 -0800

    Adding *.tf associated with aws/api

---
+++ terragrunt/aws/api/iam.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "service_principal" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "api" {
+  name               = "${var.product_name}-api"
+  assume_role_policy = data.aws_iam_policy_document.service_principal.json
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+  role       = aws_iam_role.api.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
