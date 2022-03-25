e3acc8d609a315a67d3b06f95270244a6a38b7a7
Bob Shand <bob.shand@cds-snc.ca>
2022-03-25 09:40:38 -0300

    Additional test setup and tests for short_urls not found, and redirecting to the original url

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,9 +1,37 @@
+import os
 import pytest
+
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+
+from database.db import get_db_session
+from models import Base
 
 from main import app
 
-@pytest.fixture()
-def client() -> TestClient: 
+db_engine = create_engine(os.environ.get("DATABASE_TEST_URL"))
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+
+def get_test_db_session() -> Session:
+    connection = db_engine.connect()
+    session = LocalSession()
+    try:
+        yield session
+    finally:
+        session.close()
+        connection.close()
+
+@pytest.fixture(scope="session")
+def setup_test_database():
+    Base.metadata.bind = db_engine
+    Base.metadata.create_all()
+    yield
+    Base.metadata.drop_all()
+
+@pytest.fixture(scope="session")
+def client(setup_test_database) -> TestClient:
+    app.dependency_overrides[get_db_session] = get_test_db_session
     client = TestClient(app)
     yield client
--- api/tests/test_shortener.py
+++ api/tests/test_shortener.py
@@ -1,6 +1,20 @@
 import pytest
-import fastapi.responses
+from fastapi import status
 
 def test_unknown_shorturl_returns_404(client):
     response = client.get("/doesnotexist")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+def test_creating_a_valid_shortlink(client):
+    response = client.post("/shorten", json={"original_url": "http://example.com"})
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()['status'] == 'OK'
+
+def test_known_shorturl_redirects_to_original_url(client):
+    create_response = client.post("/shorten", json={"original_url": "http://example.com"})
+    shorturl = create_response.json()['short_url']
+    
+    response = client.get(f"/{shorturl}")
+    assert response.headers['location'] == 'http://example.com'
+    assert response.status_code == status.HTTP_301_MOVED_PERMANENTLY
