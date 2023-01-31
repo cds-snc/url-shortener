13a424d3e16626fe8d2fb1ae1059bc8c3d0fd803
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-01-30 16:19:23 -0800

    Adding initial terragrunt/terraform directory structure

---
+++ terragrunt/env/common/common_variables.tf
@@ -0,0 +1,40 @@
+variable "account_id" {
+  description = "(Required) The account ID to perform actions on."
+  type        = string
+}
+
+variable "aws_org_id" {
+  description = "(Required) The AWS org account ID.  Used to limit which roles the API can assume."
+  type        = string
+  sensitive   = true
+}
+
+variable "cbs_satellite_bucket_name" {
+  description = "(Required) Name of the Cloud Based Sensor S3 satellite bucket"
+  type        = string
+}
+
+variable "domain" {
+  description = "(Required) Domain name to deploy to"
+  type        = string
+}
+
+variable "env" {
+  description = "The current running environment"
+  type        = string
+}
+
+variable "product_name" {
+  description = "(Required) The name of the product you are deploying."
+  type        = string
+}
+
+variable "region" {
+  description = "The current AWS region"
+  type        = string
+}
+
+variable "billing_code" {
+  description = "The billing code to tag our resources with"
+  type        = string
+}
---
+++ terragrunt/env/production/env_vars.hcl
@@ -0,0 +1,6 @@
+inputs = {
+  account_id       = "806721586252"
+  env              = "production"
+  cost_center_code = "url-shortener-production"
+  domain           = ""
+}
