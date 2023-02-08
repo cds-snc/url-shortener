08200d2098e531d3e194b422baafae68167fe066
Max Neuvians <max@neuvians.net>
2023-02-08 17:03:28 +0000

    fix: add DB to gitignore

--- .devcontainer/docker-entrypoint-initdb.d/create_test_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL    
-    CREATE DATABASE test;
-    GRANT ALL PRIVILEGES ON DATABASE test TO $POSTGRES_USER;
-EOSQL
