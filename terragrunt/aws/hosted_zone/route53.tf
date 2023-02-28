a1b06805611d896951cb5c0bee848f2d29aae4cf
Max Neuvians <max@neuvians.net>
2023-02-28 16:21:53 -0500

    feat: add route53 health check (#142)

--- terragrunt/aws/hosted_zone/route53.tf
+++ terragrunt/aws/hosted_zone/route53.tf
@@ -6,3 +6,16 @@ resource "aws_route53_zone" "url_shortener" {
     Terraform  = true
   }
 }
+
+resource "aws_route53_health_check" "sre_bot_healthcheck" {
+  fqdn              = aws_route53_zone.url_shortener.name
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/version"
+  failure_threshold = "3"
+  request_interval  = "30"
+
+  tags = {
+    "CostCentre" = var.billing_code
+  }
+}
