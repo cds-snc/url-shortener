340097083da8254289493b71cee72231fc77298c
Cheng Gang, Ye <cheng.ye@cds-snc.ca>
2023-03-01 15:39:01 -0800

    fix: moved ALLOWED_DOMAINS from ssm to lambda env (#146)

--- terragrunt/aws/api/lambda.tf
+++ terragrunt/aws/api/lambda.tf
@@ -17,6 +17,7 @@ module "url_shortener_lambda" {
   environment_variables = {
     API_AUTH_TOKEN_SECRET_ARN = aws_ssm_parameter.api_auth_token.arn
     SHORTENER_DOMAIN          = "https://${var.domain}/"
+    ALLOWED_DOMAINS           = "canada.ca,gc.ca"
   }
 
   policies = [
