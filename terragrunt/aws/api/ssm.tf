604879ae2380c6b6078b5a46969365f062cc7183
Cheng Gang, Ye <cheng.ye@cds-snc.ca>
2023-03-01 21:21:28 +0000

    fix: moved ALLOWED_DOMAINS from ssm to lambda env

--- terragrunt/aws/api/ssm.tf
+++ terragrunt/aws/api/ssm.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "api_auth_token" {
   name  = "api_auth_token"
   type  = "SecureString"
-  value = "API_AUTH_TOKEN=${var.api_auth_token}\nALLOWED_DOMAINS=canada.ca, gc.ca"
+  value = "API_AUTH_TOKEN=${var.api_auth_token}"
 
   tags = {
     CostCentre = var.billing_code
