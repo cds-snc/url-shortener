5f15dceab2dcbb5c364609fbe8662c84f191f046
Cheng Gang, Ye <cheng.ye@cds-snc.ca>
2023-03-16 16:47:05 -0700

    KMS key policy to allow Cloudwatch logs access (#179)
    * KMS key policy to allow Cloudwatch logs access
    
    * Fixed CKV_AWS_111
    
    * Fixed ref
    
    * formatting

---
+++ terragrunt/aws/cloudfront/iam.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "cloudfront_policies" {
+  statement {
+    sid    = "AllowKMSAccessToCloudWatchLogs"
+    effect = "Allow"
+
+    principals {
+      identifiers = [
+        "logs.${var.region}.amazonaws.com",
+      ]
+      type = "Service"
+    }
+
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*",
+    ]
+
+    resources = [
+      "arn:aws:kms:::key/*",
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values = [
+        "arn:aws:logs:${var.region}:${var.account_id}:log-group:aws-waf-logs-${var.product_name}/",
+      ]
+    }
+  }
+}
