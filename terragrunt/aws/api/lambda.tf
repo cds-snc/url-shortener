2ca304ed674b03491d715aba615d2ebed359b20e
Max Neuvians <max@neuvians.net>
2023-03-29 12:42:15 +0000

    fix: remove ARN in env

--- terragrunt/aws/api/lambda.tf
+++ terragrunt/aws/api/lambda.tf
@@ -16,8 +16,6 @@ module "url_shortener_lambda" {
 
   environment_variables = {
     ALLOWED_DOMAINS            = "canada.ca,gc.ca"
-    API_AUTH_TOKEN_SECRET_ARN  = aws_ssm_parameter.api_auth_token.arn
-    NOTIFY_API_KEY_SECRET_ARN  = aws_ssm_parameter.notify_api_key.arn
     NOTIFY_MAGIC_LINK_TEMPLATE = "c8520014-597f-4e73-8eef-baf3f5835596"
     SHORTENER_DOMAIN           = "https://${var.domain}/"
     SHORTENER_PATH_LENGTH      = "8"
