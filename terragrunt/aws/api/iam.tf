bcc05a9b73e17632762bb187b301bae960e59021
Pat Heard <patrick.heard@cds-snc.ca>
2023-04-04 19:03:47 +0000

    feat: add SSM parameters with bearer auth tokens
    Add two new SSM parameters with the App and Notify
    static Bearer tokens.

--- terragrunt/aws/api/iam.tf
+++ terragrunt/aws/api/iam.tf
@@ -24,7 +24,8 @@ data "aws_iam_policy_document" "api_policies" {
       "ssm:GetParameters",
     ]
     resources = [
-      aws_ssm_parameter.api_auth_token.arn,
+      aws_ssm_parameter.auth_token_app.arn,
+      aws_ssm_parameter.auth_token_notify.arn,
       aws_ssm_parameter.hashing_peppers.arn,
       aws_ssm_parameter.notify_api_key.arn
     ]
