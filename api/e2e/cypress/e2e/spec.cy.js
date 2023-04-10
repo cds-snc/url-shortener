550c20bb23d642f62720c69d6ec5ed56952a4ff1
Max Neuvians <max@neuvians.net>
2023-04-10 20:59:57 +0000

    fix: h1,gitignore

--- api/e2e/cypress/e2e/spec.cy.js
+++ api/e2e/cypress/e2e/spec.cy.js
@@ -1,9 +1,19 @@
 import 'cypress-axe'
 
-it("loads the login page", () => {
-    cy.visit("/login");
+it("loads the login page to cache the assets", () => {
+    cy.visit("/en/login");
+});
+
+it("loads the login page in English", () => {
+    cy.visit("/en/login");
     cy.injectAxe();
     cy.get("gcds-button").should("contain", "Login");
     cy.checkA11y()
 });
 
+it("loads the login page in Fench", () => {
+    cy.visit("/fr/login");
+    cy.injectAxe();
+    cy.get("gcds-button").should("contain", "Connexion");
+    cy.checkA11y()
+});
