517d0c986c28595ed4eb04a5a765f9227d44089e
Max Neuvians <max@neuvians.net>
2023-04-12 23:55:14 +0000

    feat: 20 users

--- api/loadtesting/test.js
+++ api/loadtesting/test.js
@@ -3,7 +3,7 @@ import { sleep } from 'k6';
 
 export const options = {
   duration: '1m',
-  vus: 5,
+  vus: 20,
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
