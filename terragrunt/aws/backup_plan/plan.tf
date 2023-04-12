434adfbba8c7ffdf269a122aa3c2e6bf1d00923e
Pat Heard <patrick.heard@cds-snc.ca>
2023-04-12 13:47:46 +0000

    fix: tf fmt

--- terragrunt/aws/backup_plan/plan.tf
+++ terragrunt/aws/backup_plan/plan.tf
@@ -6,10 +6,10 @@
 resource "aws_backup_plan" "backup_plan_default" {
   name = "backup_plan_default"
   rule {
-    rule_name           = "backup_rule_default"
-    target_vault_name   = aws_backup_vault.vault.name
-    schedule            = local.plan_schedule_default
-    start_window        = 60
+    rule_name         = "backup_rule_default"
+    target_vault_name = aws_backup_vault.vault.name
+    schedule          = local.plan_schedule_default
+    start_window      = 60
 
     lifecycle {
       cold_storage_after = local.cold_storage_after_default
