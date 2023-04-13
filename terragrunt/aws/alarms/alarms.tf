b3bb887fe1f51e09096aa1600e3da08a8dfc5d0a
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-04-13 12:54:51 -0700

    Adding files to make the alarms less noisy

--- terragrunt/aws/alarms/alarms.tf
+++ terragrunt/aws/alarms/alarms.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_metric_filter" "url_shortener_api_error" {
   name           = local.error_logged_api
-  pattern        = "?ERROR ?Error ?error ?failed"
+  pattern        = "{$.levelname=\"ERROR\"}" 
   log_group_name = local.api_cloudwatch_log_group
 
   metric_transformation {
