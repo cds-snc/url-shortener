264e729b09c8413c8d8b9a6be211e423d9fbe465
Max Neuvians <max@neuvians.net>
2023-04-13 16:19:44 +0000

    feat: delayed scenarios

--- api/loadtesting/test.js
+++ api/loadtesting/test.js
@@ -1,22 +1,42 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const HOST = __ENV.SHORTENER_DOMAIN || 'http://0.0.0.0:8000/';
+
 export const options = {
-  duration: '30s',
-  vus: 100,
+  discardResponseBodies: true,
+  scenarios: {
+    createShortUrl: {
+      executor: 'constant-vus',
+      exec: 'createShortUrl',
+      vus: 50,
+      duration: '30s',
+      gracefulStop: '0s',
+    },
+    redirectPage: {
+      executor: 'constant-vus',
+      exec: 'redirectPage',
+      vus: 100,
+      duration: '30s',
+      gracefulStop: '0s',
+      startTime: '30s',
+    },
+  },
   thresholds: {
     'http_req_failed{name:redirectPage}': ['rate<0.01'], // http errors should be less than 1%
     'http_req_duration{name:redirectPage}': ['p(95)<500'], // 95 percent of response times must be below 500ms
     'http_req_failed{name:createShortUrl}': ['rate<0.01'], // http errors should be less than 1%
-    'http_req_duration{name:createShortUrl}': ['p(95)<2500'], // 95 percent of response times must be below 2500ms
+    'http_req_duration{name:createShortUrl}': ['p(95)<2500'], // 95 percent of response times must be below 2000ms
   },
 };
 
-export default function () {
-  http.get('http://0.0.0.0:8000/foobar', { tags: { name: 'redirectPage' } });
 
+export function redirectPage() {
+  http.get(`${HOST}abcdefgh`, { tags: { name: 'redirectPage' } });
   sleep(1);
+}
 
+export function createShortUrl() {
   const data = JSON.stringify({ "original_url": "https://digital.canada.ca" })
   const params = {
     headers: {
@@ -26,6 +46,6 @@ export default function () {
     tags: { name: 'createShortUrl' }
   }
 
-  http.post('http://0.0.0.0:8000/v1', data, params);
+  http.post(`${HOST}v1`, data, params);
   sleep(1);
-};
+}
