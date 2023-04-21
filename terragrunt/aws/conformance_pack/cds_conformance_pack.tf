02ca01faf06c8b2c0c146d75abb6c41b8f661531
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-04-21 12:11:48 -0700

    Fixing formatting

--- terragrunt/aws/conformance_pack/cds_conformance_pack.tf
+++ terragrunt/aws/conformance_pack/cds_conformance_pack.tf
@@ -1,5 +1,5 @@
 module "conformance_pack" {
-  source 							= "github.com/cds-snc/terraform-modules?ref=v5.1.6//cds_conformance_pack"
+  source                                                        = "github.com/cds-snc/terraform-modules?ref=v5.1.6//cds_conformance_pack"
   internet_gateway_authorized_vpc_only_param_authorized_vpc_ids = var.vpc_id
-  billing_tag_value 						= var.billing_code
+  billing_tag_value                                             = var.billing_code
 }
