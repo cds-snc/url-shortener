555aa8e5d2c46f7e686e4e352fec83b32e90a5ae
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-04-24 09:16:18 -0700

    Adding excluded rules from conformance pack

--- terragrunt/aws/conformance_pack/cds_conformance_pack.tf
+++ terragrunt/aws/conformance_pack/cds_conformance_pack.tf
@@ -1,5 +1,6 @@
 module "conformance_pack" {
-  source                                                        = "github.com/cds-snc/terraform-modules?ref=v5.1.6//cds_conformance_pack"
+  source                                                        = "github.com/cds-snc/terraform-modules?ref=v5.1.8//cds_conformance_pack"
   internet_gateway_authorized_vpc_only_param_authorized_vpc_ids = var.vpc_id
+  excluded_rules                                                = ["LambdaDlqCheck", "LambdaFunctionPublicAccessProhibited", "S3BucketLoggingEnabled"]
   billing_tag_value                                             = var.billing_code
 }
