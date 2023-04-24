3b946c4ef19b06a4375522503194a8fdd6cdc2f9
Pat Heard <patrick.heard@cds-snc.ca>
2023-04-24 17:24:13 +0000

    feat: add DDoS alarms for CloudFront/Route53
    Add CloudWatch alarms that trigger if AWS Shield Advanced detects
    a DDoS attack against the API's CloudFront distribution or
    Route53 records.

--- terragrunt/aws/alarms/sns.tf
+++ terragrunt/aws/alarms/sns.tf
@@ -3,10 +3,34 @@
 #
 resource "aws_sns_topic" "cloudwatch_warning" {
   name = "cloudwatch-alarms-warning"
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+resource "aws_sns_topic" "cloudwatch_warning_us_east" {
+  provider = aws.us-east-1
+
+  name = "cloudwatch-alarms-warning"
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
 }
 
 resource "aws_sns_topic_subscription" "alert_warning" {
   topic_arn = aws_sns_topic.cloudwatch_warning.arn
+  protocol  = "lambda"
+  endpoint  = module.cloudwatch_alarms_slack.lambda_arn
+}
+
+resource "aws_sns_topic_subscription" "alert_warning_us_east" {
+  provider = aws.us-east-1
+
+  topic_arn = aws_sns_topic.cloudwatch_warning_us_east.arn
   protocol  = "lambda"
   endpoint  = module.cloudwatch_alarms_slack.lambda_arn
 }
