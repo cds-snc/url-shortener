f48cd6e8559c2f58fe53b5903b48f4d15e99804f
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-04-25 14:15:44 -0700

    Change backup lengths for cold storage and deletion (#314)
    * Reducing the lenght of the cold storage and delete
    
    * Fixing formatting

--- terragrunt/aws/backup_plan/locals.tf
+++ terragrunt/aws/backup_plan/locals.tf
@@ -3,6 +3,6 @@ locals {
   # Default Backup plan
   plan_schedule_default         = "cron(0 12 * * ? *)" # every day at 12:00
   plan_schedule_default_testing = "cron(0 12 * * ? *)" # once every 10 minutes for testing
-  cold_storage_after_default    = 30
-  delete_after_default          = 120
+  cold_storage_after_default    = 7
+  delete_after_default          = 14
 }
