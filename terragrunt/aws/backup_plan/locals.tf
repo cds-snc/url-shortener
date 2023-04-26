3a7e32fefe59a2a3c5f8195ce0997f48ed8a02bf
Pat Heard <patrick.heard@cds-snc.ca>
2023-04-26 17:05:45 +0000

    fix: DynamoDB backup plan
    The delete must be 90 days after the cold storage date.

--- terragrunt/aws/backup_plan/locals.tf
+++ terragrunt/aws/backup_plan/locals.tf
@@ -4,5 +4,5 @@ locals {
   plan_schedule_default         = "cron(0 12 * * ? *)" # every day at 12:00
   plan_schedule_default_testing = "cron(0 12 * * ? *)" # once every 10 minutes for testing
   cold_storage_after_default    = 7
-  delete_after_default          = 14
+  delete_after_default          = 97 # Must be 90 days or more after cold storage
 }
