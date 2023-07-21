f854e247c65638e9cdc736504530187e8c3f3a87
Pat Heard <patrick.heard@cds-snc.ca>
2023-07-21 16:03:48 -0400

    fix: use SRE Bot to post Slack notifications
    Update to use the SRE Bot to post CloudWatch alarm
    Slack notifications.

--- terragrunt/aws/alarms/sns.tf
+++ terragrunt/aws/alarms/sns.tf
@@ -23,14 +23,14 @@ resource "aws_sns_topic" "cloudwatch_warning_us_east" {
 
 resource "aws_sns_topic_subscription" "alert_warning" {
   topic_arn = aws_sns_topic.cloudwatch_warning.arn
-  protocol  = "lambda"
-  endpoint  = module.cloudwatch_alarms_slack.lambda_arn
+  protocol  = "https"
+  endpoint  = var.slack_webhook_url
 }
 
 resource "aws_sns_topic_subscription" "alert_warning_us_east" {
   provider = aws.us-east-1
 
   topic_arn = aws_sns_topic.cloudwatch_warning_us_east.arn
-  protocol  = "lambda"
-  endpoint  = module.cloudwatch_alarms_slack.lambda_arn
+  protocol  = "https"
+  endpoint  = var.slack_webhook_url
 }
